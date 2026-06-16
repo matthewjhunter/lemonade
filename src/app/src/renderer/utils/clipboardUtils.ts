b3192560edf02c062ecb3af933e6ac42cb732660
Florian Reinle <f.reinle@otec.de>
2026-06-16 10:43:35 +0000

    remove old src/app

--- src/app/src/renderer/utils/clipboardUtils.ts
+++
@@ -1,7 +0,0 @@
-export async function writeClipboard(text: string): Promise<void> {
-  if (window.api?.writeClipboard) {
-    await window.api.writeClipboard(text);
-  } else {
-    await navigator.clipboard.writeText(text);
-  }
-}
